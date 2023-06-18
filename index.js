b5a936e5235fe69696389981d56e94a8abc46c41
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-06-18 16:17:24 +0300

    minor  changes

--- index.js
+++ index.js
@@ -12,67 +12,68 @@ let canAddBet = true;
 let data = {};
 data.bet = 0;
 
-    // disable right click
-    document.addEventListener('contextmenu', event => event.preventDefault());
- 
-    document.onkeydown = function (e) {
- 
-        // disable F12 key
-        if(e.keyCode == 123) {
-            return false;
-        }
- 
-        // disable I key
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-            return false;
-        }
- 
-        // disable J key
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-            return false;
-        }
- 
-        // disable U key
-        if(e.ctrlKey && e.keyCode == 85) {
-            return false;
-        }
-    }
+// disable right click
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function (e) {
+// disable F12 key
+if (e.keyCode == 123) {
+ return false;
+}
+// disable I key
+if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+ return false;
+}
+// disable J key
+if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+ return false;
+}
+// disable U key
+if (e.ctrlKey && e.keyCode == 85) {
+ return false;
+}
+}
 
 window.onload = function () {
     buildDeck();
     addChips();
 }
+
 document.getElementById("add-chips").addEventListener("click", addMoreChips);
-function addMoreChips () {
+function addMoreChips() {
     chips += 10;
     let totalChips = document.getElementById("total-chips");
     totalChips.innerText = parseInt(chips);
 }
+
 function addChips() {
     let totalChips = document.getElementById("total-chips");
     totalChips.innerText = parseInt(chips);
 }
+
 const betEl = document.getElementById("place-bet");
 betEl.addEventListener("submit", event => {
     event.preventDefault();
     if (canAddBet) {
-    const betData = new FormData(betEl);
-    data = Object.fromEntries(betData);
-    chips -= Number(data.bet);
-    let totalChips = document.getElementById("total-chips");
-    totalChips.innerText = parseInt(chips);
-    console.log(data.bet);
-    startGame();
-    isBetPlaced = true;
-    canAddBet = false;
-}
+        const betData = new FormData(betEl);
+        data = Object.fromEntries(betData);
+        chips -= Number(data.bet);
+        let totalChips = document.getElementById("total-chips");
+        totalChips.innerText = parseInt(chips);
+        console.log(data.bet);
+        startGame();
+        isBetPlaced = true;
+        canAddBet = false;
+    }
 })
+
 let value = document.querySelector("#value");
 let input = document.querySelector("#bet");
 value.textContent = input.value;
+
 input.addEventListener("input", (event) => {
     value.textContent = event.target.value;
 })
+
 function buildDeck() {
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J"];
     let suits = ["C", "H", "S", "D"];
@@ -83,6 +84,7 @@ function buildDeck() {
     }
     deck = deck.sort((a, b) => 0.5 - Math.random()) // shuffle the deck
 }
+
 function startGame() {
     document.getElementById("dealer-sum").innerText = dealerSum;
     let hiddenCardImg = document.createElement("img");
@@ -94,11 +96,12 @@ function startGame() {
     while (dealerSum < 17) { //draw card for the dealer 
         let cardImg = document.createElement("img");
         let card = deck.pop();
-       cardImg.src = "img/cards/" + card + ".png";
+        cardImg.src = "img/cards/" + card + ".png";
         dealerSum += getValue(card);
         dealerAces += checkAce(card);
         document.getElementById("dealer-cards").append(cardImg);
     }
+
     for (let i = 0; i < 2; i++) { //draw 2 cards for the player
         let cardImg = document.createElement("img");
         let card = deck.pop();
@@ -109,7 +112,9 @@ function startGame() {
         document.getElementById("player-sum").innerText = playerSum;
     }
 }
+
 document.getElementById("hit").addEventListener("click", hit);
+
 function hit() {
     if (playerSum > 21) {
         canHit = false;
@@ -131,7 +136,9 @@ function hit() {
         }
     }
 }
+
 document.getElementById("stay").addEventListener("click", stay);
+
 function stay() {
     if (isBetPlaced && canStay) {
         document.getElementById("hidden").src = "img/cards/" + hidden + ".png"
@@ -155,6 +162,7 @@ function stay() {
     canHit = false;
     addChips();
 }
+
 function getValue(card) {
     let data = card.split("-")
     let value = data[0];
@@ -168,13 +176,16 @@ function getValue(card) {
         return parseInt(value);
     }
 }
+
 function checkAce(card) {
     if (card[0] === "A") {
         return 1;
     }
     return 0;
 }
+
 document.getElementById("new-hand").addEventListener("click", newHand);
+
 function newHand() {
     while (deck.length > 0) {
         deck.pop();
@@ -196,10 +207,12 @@ function newHand() {
     isBetPlaced = false;
     canStay = true;
     canAddBet = true;
-   // document.getElementById("dealer-sum").innerText = dealerSum;
+    // document.getElementById("dealer-sum").innerText = dealerSum;
     buildDeck();
 }
+
 document.getElementById("restart").addEventListener("click", restart);
+
 function restart() {
     location.reload();
 } 
